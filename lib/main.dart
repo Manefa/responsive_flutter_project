b796f4bc262a26f2174af6ba1b590220e1a5d11c
manefa <manefae8@gmail.com>
2022-06-02 08:57:15 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:untitled/lib/courses_page.dart';
+
+void main() => runApp(const AppWidget());
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget!),
+        breakpoints: const [
+          ResponsiveBreakpoint.resize(350, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(600, name: TABLET),
+          ResponsiveBreakpoint.resize(800, name: DESKTOP),
+          ResponsiveBreakpoint.autoScale(1700, name: 'XL'),
+        ],
+      ),
+      title: 'Flutter Responsive Framework',
+      home: const CoursesPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
